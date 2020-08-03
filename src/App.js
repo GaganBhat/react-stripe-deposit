9311b58ebd100dbbe0fbe4b4a7d4510e051a48a7
Gagan Bhat <gaganbhat6@gmail.com>
2020-08-03 17:43:44 -0500

    Firebase config added

--- src/App.js
+++ src/App.js
@@ -7,27 +7,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bet America
         </a>
+        <a>Under Construction</a>
       </header>
-      <!-- The core Firebase JS SDK is always required and must be listed first -->
-      <script src="/__/firebase/7.17.1/firebase-app.js"></script>
-
-      <!-- TODO: Add SDKs for Firebase products that you want to use
-           https://firebase.google.com/docs/web/setup#available-libraries -->
-      <script src="/__/firebase/7.17.1/firebase-analytics.js"></script>
-
-      <!-- Initialize Firebase -->
-      <script src="/__/firebase/init.js"></script>
     </div>
   );
 }
