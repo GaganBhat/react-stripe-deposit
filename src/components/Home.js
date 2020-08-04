36918b7194de9ee273520e8f1a2e7b48fc85b1bf
Gagan Bhat <gaganbhat6@gmail.com>
2020-08-04 18:54:38 -0500

    Centered home as well

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,10 @@ import app from "../base";
 
 const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
+    <div style={{width: "30%", margin: "0 auto"}}>
+      <h1>You are Logged In</h1>
       <button onClick={() => app.auth().signOut()}>Sign out</button>
-    </>
+    </div>
   );
 };
 
