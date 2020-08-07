d6bb4c8ec291fa84544c49b9a326d773e553513d
Gagan Bhat <gaganbhat6@gmail.com>
2020-08-07 14:28:21 -0500

    Working create customer for react

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
-const port = process.env.APIPORT || 3000;
+const port = process.env.APIPORT || 4000;
+const { stripeService } = require('./services/stripe.js');
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send(`Backend API is running on port ${port}.`);
 });
 
-app.post("/stripe/createcustomer/", (req, res) => {
-
+app.post("/stripe/createcustomer", async (req, res) => {
+  const customer = await stripeService.CreateCustomer(req.body);
+  res.json(customer);
 });
 
 app.listen(port, () => {console.log(`Started express server on port ${port}.`)});
-
 
--- src/services/stripe.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {loadStripe} from '@stripe/stripe-js';
-
-const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
-
-export const CreateStripeCustomer  = () => {
-
-}
